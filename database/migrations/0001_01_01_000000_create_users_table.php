5bad23b1bdebfdfc024e9cd2804ed25bfc1f7d8b
Andreaaa <andrea.wijaya@binus.ac.id>
2024-05-30 13:13:33 +0700

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('profile_pics')->nullable();
-            $table->string('role')->default('user');
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
