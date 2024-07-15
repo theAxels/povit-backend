547b6f9c7af2a07b1553d29e31fb67303b3176aa
theAxels <127481633+theAxels@users.noreply.github.com>
2024-07-15 12:51:26 +0700

    Revert "Merge pull request #29 from theAxels/main"
    This reverts commit 4771905def1d76e7415859f533ba702c87de2fb3, reversing
    changes made to f3c924946254666cf4e7000fabe623977e577819.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,127 +7,91 @@
 @endsection
 
 @section('dashboard')
-<div class="content-container">
-    <div class="content">
-        <div class="camera w-100 d-flex flex-column justify-content-center p-1 align-items-center" style="height: 100vh;">
-            <div class="center-box" id="kamera">
-                <div id="my_camera"></div>
-                <div class="control-bar p-3">
-                    <span class="material-symbols-outlined image-icon" style="font-size: 3rem;">
-                        image
-                        <input type="file" id="fileInput" accept="image/*">
-                    </span>
-                    <input type="button" class="circle-btn" onClick="take_snapshot()"></input>
-                </div>
+    <div class="camera w-100 d-flex flex-column justify-content-center p-1 align-items-center" style="margin-left: 70px;">
+        <div class="center-box" id="kamera">
+            <div id="my_camera"></div>
+            <div class="control-bar p-3">
+                <span class="material-symbols-outlined image-icon" style="font-size: 3rem;">
+                    image
+                    <input type="file" id="fileInput" accept="image/*">
+                </span>
+                <input type="button" class="circle-btn" onClick="take_snapshot()"></input>
             </div>
-            <div class="history-text" id="historyArrow">
-                <button onclick="scrollDown()" style="border: none; background: none; outline: none;">
-                    <p class="mb-0">History</p>
-                    <span class="material-symbols-outlined">
-                        keyboard_double_arrow_down
-                    </span>
-                </button>
-            </div>
-            <form id="hasil" class="h-100 w-100 flex-column justify-content-center align-items-center" style="display: none;" method="POST" action="{{ route('post_image') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="d-flex flex-row align-items-center justify-content-between py-1 position-relative w-100 mb-4">
-                    <div style="flex: 1; text-align: center;">
-                        <h5>Sent to...</h5>
-                    </div>
-                    <button type="button" title="Save Images" style="border: none; background: none; outline: none; position: absolute; right: 18%;" id="downloadCaptured">
-                    </button>
-                </div>
-                <div class="center-box d-flex flex-column align-items-center" id="results" style="border: 2px solid #000000;">
-                    <button type="button" style="border: none; background: none; outline: none; position: absolute; left: 10px; top: 10px;" onclick="showCamera()">
-                        <i class="fa-solid fa-circle-xmark" style="color: #000000; font-size: 20px;"></i>
-                    </button>
-                    <div id="carouselExample" class="carousel slide position-absolute w-70" style="bottom: 20px;">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <div class="form-group">
-                                    <input type="text" name="caption" class="form-control caption-input" placeholder="Add a message ...">
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="form-group d-flex flex-row justify-content-center">
-                                    <input type="text" name="location" class="form-control  location-input" id="locationInput" placeholder="Add a location ...">
-                                    <button type="button" class="location-button" onclick="getCurrentLocation()" style="border: none; outline: none;">
-                                        <span class="material-symbols-outlined">my_location</span>
-                                    </button>
-                                </div>
-                                <div class="d-block w-100">
-                                </div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-
-                </div>
-                <input type="file" class="d-none" name="pict" id="pictInput" accept="image/*">
-                <div class="d-flex flex-row justify-content-between align-items-center mt-4 w-100" style="max-width: 500px;">
-                    <div id="all" class="d-flex flex-column justify-content-center align-items-center w-100">
-                        <input type="radio" id="group-radio" name="is_closed_friend" class="radio-btn" value="false" checked>
-                        <label for="group-radio" class="circleButton d-flex justify-content-center align-items-center">
-                            <span class="material-symbols-outlined">group</span>
-                        </label>
-                        <h6 class="mt-1">All</h6>
-                    </div>
-                    <div class="d-flex justify-content-center align-items-center w-100">
-                        <button type="submit" title="Create Post" class="send-btn d-flex justify-content-center align-items-center">
-                            <span class="material-symbols-outlined" style="font-size: 280%">send</span>
-                        </button>
-                    </div>
-                    <div id="cf" class="d-flex flex-column justify-content-center align-items-center w-100">
-                        <input type="radio" id="star-radio" name="is_closed_friend" class="radio-btn" value="true">
-                        <label for="star-radio" class="circleButton d-flex justify-content-center align-items-center">
-                            <span class="material-symbols-outlined">star</span>
-                        </label>
-                        <h6 class="mt-1">Close Friends</h6>
-                    </div>
-                </div>
-            </form>
         </div>
-        @foreach ($posts as $post)
-            <div class="camera w-100 d-flex flex-column justify-content-center p-1 align-items-center" style="height: 100vh;">
-                <div class="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
-                    {{-- <div class="d-flex flex-row align-items-center justify-content-between py-1 position-relative w-100 mb-4">
-                        <div style="flex: 1; text-align: center;">
-                            <h5>Sent to...</h5>
+        <div class="history-text" id="historyArrow">
+            <a href="#">
+                <p class="mb-0">History</p>
+                <span class="material-symbols-outlined">
+                    keyboard_double_arrow_down
+                </span>
+            </a>
+        </div>
+        <form id="hasil" class="h-100 w-100 flex-column justify-content-center align-items-center" style="display: none;" method="POST" action="{{ route('post_image') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="d-flex flex-row align-items-center justify-content-between py-1 position-relative w-100 mb-4">
+                <div style="flex: 1; text-align: center;">
+                    <h5>Sent to...</h5>
+                </div>
+                <button type="button" title="Save Images" style="border: none; background: none; outline: none; position: absolute; right: 18%;" id="downloadCaptured">
+                </button>
+            </div>            
+            <div class="center-box d-flex flex-column align-items-center" id="results" style="border: 2px solid #000000;">
+                <button type="button" style="border: none; background: none; outline: none; position: absolute; left: 10px; top: 10px;" onclick="showCamera()">
+                    <i class="fa-solid fa-circle-xmark" style="color: #000000; font-size: 20px;"></i>
+                </button>
+                <div id="carouselExample" class="carousel slide position-absolute w-70" style="bottom: 20px;">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="form-group">
+                                <input type="text" name="caption" class="form-control caption-input" placeholder="Add a message ...">
+                            </div>
                         </div>
-                    </div>             --}}
-                    <div class="text-center">
-                        <h2>{{ $post->sender->name }}</h2>
+                        <div class="carousel-item">
+                            <div class="form-group d-flex flex-row justify-content-center">
+                                <input type="text" name="location" class="form-control  location-input" id="locationInput" placeholder="Add a location ...">
+                                <button type="button" class="location-button" onclick="getCurrentLocation()" style="border: none; outline: none;">
+                                    <span class="material-symbols-outlined">my_location</span>
+                                </button>
+                            </div>
+                            <div class="d-block w-100">
+                            </div>
+                        </div>
                     </div>
-                    <div class="center-box d-flex flex-column align-items-center"style="border: 2px solid #000000;">
-                        <img src="{{ asset('user_post/' . $post->pict) }}" alt="" style="width: 100%; height: 100%; object-fit: cover;">
-                    </div>
-                    <p>{{ $post->caption }}</p>
-                    <p>Location: {{ $post->location }}</p>
-                    <p>Posted at: {{ $post->created_at }}</p>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>  
+                         
+            </div>
+            <input type="file" class="d-none" name="pict" id="pictInput" accept="image/*">
+            <div class="d-flex flex-row justify-content-evenly align-items-center mt-4 w-100">
+                <div id="all" class="d-flex flex-column justify-content-center align-items-center">
+                    <input type="radio" id="group-radio" name="is_closed_friend" class="radio-btn" value="false" checked>
+                    <label for="group-radio" class="circleButton d-flex justify-content-center align-items-center">
+                        <span class="material-symbols-outlined">group</span>
+                    </label>
+                    <h6 class="mt-1">All</h6>
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+                    <button type="submit" title="Create Post" class="send-btn d-flex justify-content-center align-items-center">
+                        <span class="material-symbols-outlined" style="font-size: 280%">send</span>
+                    </button> 
+                </div> 
+                <div id="cf" class="d-flex flex-column justify-content-center align-items-center">
+                    <input type="radio" id="star-radio" name="is_closed_friend" class="radio-btn" value="true">
+                    <label for="star-radio" class="circleButton d-flex justify-content-center align-items-center">
+                        <span class="material-symbols-outlined">star</span>
+                    </label>
+                    <h6 class="mt-1">Close Friends</h6>
                 </div>
             </div>
-        @endforeach
+        </form>
     </div>
-    <div class="page-control">
-        <button class="scroll-button scroll-up" data-label="Previous Post" onclick="scrollUp()">
-            <span class="material-symbols-outlined">
-                arrow_upward
-            </span>
-        </button>
-        <button class="scroll-button scroll-down" data-label="Next Post" onclick="scrollDown()">
-            <span class="material-symbols-outlined">
-                arrow_downward
-            </span>
-        </button>
-    </div>
-</div>
 @endsection
 
 @section('closeFriend')
