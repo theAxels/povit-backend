b44ff9eb423ab94316768b34b9c428b7489d680a
Andreaaa <andrea.wijaya@binus.ac.id>
2024-07-21 22:25:42 +0700

    modal History Done

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -11,7 +11,7 @@
         .photo-container {
             position: relative;
             width: 100%;
-            padding-top: 100%; 
+            padding-top: 100%;
             overflow: hidden;
         }
         .photo-container img {
@@ -21,6 +21,23 @@
             width: 100%;
             height: 100%;
             object-fit: cover;
+            cursor: pointer;
+        }
+        .modal-dialog-centered {
+            display: flex;
+            align-items: center;
+        }
+        .modal-content {
+            max-width: 30%;
+            max-height: 30%;
+            margin: auto;
+        }
+        .modal-body {
+            padding: 0;
+        }
+        #modalImage {
+            width: 100%;
+            height: auto;
         }
     </style>
 </head>
@@ -32,16 +49,36 @@
             @foreach($posts as $post)
                 <div class="col-4 col-md-2 mb-4">
                     <div class="photo-container">
-                        <img src="{{ asset('user_post/' . $post->pict) }}" alt="Photo">
+                        <img src="{{ asset('user_post/' . $post->pict) }}" alt="Photo" data-toggle="modal" data-target="#photoModal" data-src="{{ asset('user_post/' . $post->pict) }}">
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
+
+
+    <div class="modal fade" id="photoModal" tabindex="-1" aria-labelledby="photoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <img id="modalImage" src="" alt="Photo" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
     @extends('main.closefriend')
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        $('#photoModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var src = button.data('src');
+            var modal = $(this);
+            modal.find('#modalImage').attr('src', src);
+        });
+    </script>
 </body>
 </html>
