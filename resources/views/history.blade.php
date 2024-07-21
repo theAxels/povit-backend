8ddc8c323691b444cbd67d466113fc4554e944a1
Andreaaa <andrea.wijaya@binus.ac.id>
2024-07-21 21:43:15 +0700

    ini integrate be ke FE gallery

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -9,38 +9,35 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .photo-placeholder {
-            background-color: #e0e0e0;
-            width: 100%;
-            padding-bottom: 4%;
             position: relative;
+            width: 100%;
+            padding-bottom: 100%; /* This maintains a 1:1 aspect ratio */
+            background-color: #e0e0e0;
         }
-        .photo-placeholder::before {
-            content: '';
-            display: block;
-            padding-top: 100%;
+        .photo-placeholder img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
         }
     </style>
 </head>
 <body>
     <div class="container mt-0">
-
         <h1 class="mb-4">Your Photos</h1>
         <div class="row">
-            <div class="col-4 col-md-2 mb-4">
-                <div class="photo-placeholder"></div>
-            </div>
-            <div class="col-4 col-md-2 mb-4">
-                <div class="photo-placeholder"></div>
-            </div>
-            <div class="col-4 col-md-2 mb-4">
-                <div class="photo-placeholder"></div>
-            </div>
-            <div class="col-4 col-md-2 mb-4">
-                <div class="photo-placeholder"></div>
-            </div>
+            @foreach($posts as $post)
+                <div class="col-4 col-md-2 mb-4">
+                    <div class="photo-placeholder">
+                        <img src="{{ $post->image_path }}" alt="Photo">
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-    @include('main.closefriend')
+    @extends('main.closefriend')
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
